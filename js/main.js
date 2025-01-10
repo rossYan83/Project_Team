ce5f76d631c3ee4c863edfd74f93ff728a3e0dd3
Ros Yan <ros.yanikin@gmail.com>
2025-01-10 19:54:30 +0000

    new

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.section-3__slider').slick({
+        prevArrow: '<img class="section-3__slider-arrow slider__arrow-left" src="./images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="section-3__slider-arrow slider__arrow-right" src="./images/arrow-right.svg" alt="">',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+
+    });
+
+    
+
+  
+});
